b286f79a26fe1e2b1373aac3c1411ecf1ad60b73
aenlemmea <118059484+aenlemmea@users.noreply.github.com>
2024-06-29 16:41:27 +0000

    Added client files

---
+++ client/src/components/VideoCard.jsx
@@ -0,0 +1,15 @@
+/* eslint react/prop-types: 0*/
+
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@mui/material";
+
+export default function VideoCard(props) {
+  const imageURL = props.imageURL;
+  const title = props.title;
+  const author = props.author;
+}
