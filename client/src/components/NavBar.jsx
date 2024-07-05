9e9925391b49a7478497e4878597b403f0ce7dd1
aenlemmea <118059484+aenlemmea@users.noreply.github.com>
2024-07-05 15:59:00 +0000

    Added NavBar

---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import Lens from "../assets/images/lens.png";
+import "./NavBar.css";
+
+export default function NavBar() {
+    return (
+        <div className="nav-div">
+             <nav>
+                <img src={Lens} />
+            </nav>
+            <h1>LEISCOPE</h1>
+        </div>
+    )
+}
