51164a0098c9e5e7a7dbd301713c92939933c9c9
aenlemmea <118059484+aenlemmea@users.noreply.github.com>
2024-07-07 12:53:45 +0000

    I try and try and try to fix spaces!

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,52 @@
 import "./App.css";
 import NavBar from "./components/NavBar";
-
+import VideoCard from "./components/VideoCard";
+import ThumbNail from "./assets/images/thumbnail.webp";
+import BoyHeron from "./assets/images/boyheron.jpg";
+import Fog from "./assets/images/fog.jpg";
+import Jungle from "./assets/images/jungle.jpg";
+import Sin from "./assets/images/sin.jpg";
+// TODO: Figure out why container spacing is not working as expected. Then remove the ugly sx hack in grid.
+// TODO: Fix the grid layout so that the spacing between the cards is consistent and zooming out too much doesn't break the view. Currently it does break it.
+// TODO: Remove all the image import after setting up the database. Currently only just testing.
+// TODO: Experiment with grid layouts. Either with Material UI or just css and see if something other than flexbox works and looks good enough. Currently the cards look too fat.
 function App() {
   return (
     <>
       <div>
         <NavBar />
+        <div className="grid-leiscope">
+          <VideoCard
+            title="The First Omen"
+            author="Arkasha Stevenson"
+            imageURL={ThumbNail}
+            height="400px"
+          />
+          <VideoCard
+            title="Sinchan: Kung Fu Boys"
+            author="Wataru Takahashi"
+            imageURL={Sin}
+            height="400px"
+          />
+          <VideoCard
+            title="The Boy and The Heron"
+            author="Hayao Miyazaki"
+            imageURL={BoyHeron}
+            height="400px"
+          />
+          <VideoCard
+            title="The Fog"
+            author="Rupert Wainwright"
+            imageURL={Fog}
+            height="400px"
+          />
+          <VideoCard
+            title="The Jungle Bunch 2"
+            author="Yannick Moulin & Benoit Somville"
+            imageURL={Jungle}
+            height="400px"
+          />
+        </div>
       </div>
     </>
   );
--- client/src/components/VideoCard.jsx
+++ client/src/components/VideoCard.jsx
@@ -1,15 +1,50 @@
 /* eslint react/prop-types: 0*/
-
 import {
+  Button,
   Card,
   CardActionArea,
+  CardActions,
   CardContent,
   CardMedia,
   Typography,
 } from "@mui/material";
 
+// TODO: Fix the white outline when the card is selected
 export default function VideoCard(props) {
   const imageURL = props.imageURL;
   const title = props.title;
   const author = props.author;
+  const height = props.height;
+  return (
+    <Card
+      sx={{
+        backgroundColor: "black",
+        color: "white",
+        width: "22%",
+      }}
+    >
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height={height}
+          image={imageURL}
+          alt={title}
+          sx={{ objectFit: "fill" }}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {title}
+          </Typography>
+          <Typography variant="body2" color="grey">
+            {author}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+      </CardActions>
+    </Card>
+  );
 }
