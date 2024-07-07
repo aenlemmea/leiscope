d578eff7d8d153d197ef5734ea8e493b36edd5b7
Soumi Karmakar <128319203+godsoumi@users.noreply.github.com>
2024-07-07 11:07:44 +0530

    change navbar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,13 +1,25 @@
 import Lens from "../assets/images/lens.png";
 import "./NavBar.css";
 
+
 export default function NavBar() {
-    return (
+     return (
         <div className="nav-div">
              <nav>
-                <img src={Lens} />
+            <img src={Lens} className="logo"/>
             </nav>
             <h1>LEISCOPE</h1>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Movie</li>
+                <li>Latest Movie</li>
+            </ul>
+
+            <div className="search-box">
+                <input type="text"placeholder="search" />
+            </div>
         </div>
-    )
+)
+    
 }
