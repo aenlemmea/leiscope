281a5bf2b98878611fd16c95ed2f996d0ad113b2
aenlemmea <118059484+aenlemmea@users.noreply.github.com>
2024-07-07 09:17:30 +0000

    Fix Navbar for responsive design

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,8 +11,4 @@ function App() {
   );
 }
 
-<<<<<<< HEAD
 export default App;
-=======
-export default App
->>>>>>> 0311a0989e6d8a957851bba42f03f7e1506e81be
--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,36 +1,17 @@
 import Lens from "../assets/images/lens.png";
 import "./NavBar.css";
 
-
 export default function NavBar() {
-<<<<<<< HEAD
   return (
     <div className="nav-div">
       <nav>
-        <img src={Lens} />
+        <img src={Lens} className="logo" />
       </nav>
       <h1>LEISCOPE</h1>
+
+      <div className="search-box">
+        <input type="text" placeholder="Search" />
+      </div>
     </div>
   );
-=======
-     return (
-        <div className="nav-div">
-             <nav>
-            <img src={Lens} className="logo"/>
-            </nav>
-            <h1>LEISCOPE</h1>
-            <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>Movie</li>
-                <li>Latest Movie</li>
-            </ul>
-
-            <div className="search-box">
-                <input type="text"placeholder="search" />
-            </div>
-        </div>
-)
-    
->>>>>>> 0311a0989e6d8a957851bba42f03f7e1506e81be
 }
